f863803e0f70299c9a16b946b130de9d1e51b86b
Maycon <mayconbruno118@gmail.com>
2025-06-23 12:37:07 -0300

    Primeiro commit

---
+++ resources/js/Pages/Auth/Register.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import Container from '../../Components/Container.vue';
+    import Title from '../../Components/Title.vue';
+    import TextLink from '../../Components/TextLink.vue';
+</script>
+
+<template>  
+    <Container>
+        <div class="mb-8 text-center">
+            <Title>Register a new account</Title>
+            <p>Already have an account? <TextLink routeName="home" label="Login"/></p>
+        </div>
+    </Container>
+
+</template>
