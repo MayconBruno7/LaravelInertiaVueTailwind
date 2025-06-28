145ebb86254694b35d668f9026b4c661643b8a02
Maycon <mayconbruno118@gmail.com>
2025-06-28 13:21:58 -0300

    Rota auth, Componentes: CheckBox, ErrorMessages, InputField, PrimaryBtn, usando shared para expor o id e nome do usuário logado para assim tornar dinamico os menus da barra de navegação

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth as FacadesAuth;
 use Inertia\Inertia;
 
 class RegisterController extends Controller
@@ -11,5 +13,27 @@ class RegisterController extends Controller
     public function create()
     {
         return Inertia::render('Auth/Register');
+    }
+
+    public function store(Request $request)
+    {
+        sleep(2);
+
+        $credentials = $request->validate([
+            'name' => 'required',
+            'email' => 'required|lowercase|email|max:255',
+            'password' => 'required|confirmed|min:3'
+        ]);
+
+        $user = User::create($credentials);
+
+        // Auth::Login($user);
+
+        FacadesAuth::Login($user);
+
+        // Auth::
+
+        return redirect()->route('home');
+        // dd($request);
     }
 }
---
+++ routes/auth.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthenticateController;
+use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Support\Facades\Route as Route;
+
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [RegisterController::class, 'create'])->name('register');
+    Route::post('/register', [RegisterController::class, 'store']);
+
+    Route::get('/login', [AuthenticateController::class, 'create'])->name('login');
+    Route::post('/login', [AuthenticateController::class, 'store']);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [AuthenticateController::class, 'destroy'])->name('logout');
+});
